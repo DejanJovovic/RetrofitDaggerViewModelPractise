39e37bbbec66a966026ab60a3afc055ca0f875f1
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-09-25 10:26:27 +0200

    update movies functionality

--- app/src/main/java/com/deki/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/deki/tmdbclient/presentation/movie/MovieActivity.kt
@@ -2,6 +2,9 @@ package com.deki.tmdbclient.presentation.movie
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -37,27 +40,57 @@ class MovieActivity : AppCompatActivity() {
         initRecyclerView()
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
         adapter = MovieAdapter()
         binding.movieRecyclerView.adapter = adapter
         displayPopularMovies()
     }
 
-    private fun displayPopularMovies(){
+    private fun displayPopularMovies() {
 
         binding.movieProgressBar.visibility = View.VISIBLE
 
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            if(it != null){
+            if (it != null) {
                 adapter.setList(it)
                 adapter.notifyDataSetChanged()
                 binding.movieProgressBar.visibility = View.GONE
-            }
-            else {
+            } else {
                 binding.movieProgressBar.visibility = View.GONE
                 Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
             }
         })
     }
